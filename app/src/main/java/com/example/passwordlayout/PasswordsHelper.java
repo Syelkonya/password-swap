7cd25ac15471a2f96b5cc8171d606683b6357eaa
Syelkonya <55093905+Syelkonya@users.noreply.github.com>
2019-11-15 21:34:01 +0300

    Привел словарь к мапу

--- app/src/main/java/com/example/passwordlayout/PasswordsHelper.java
+++ app/src/main/java/com/example/passwordlayout/PasswordsHelper.java
@@ -1,17 +1,23 @@
 package com.example.passwordlayout;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PasswordsHelper {
 
-    private final String[] mRussians;
-    private final String[] mLatins;
+
+    private final Map<Character, Character> russianToLatin = new HashMap<>();
 
     public PasswordsHelper(String[] russians, String[] latins) {
         if (russians.length != latins.length) {
             throw new IllegalArgumentException();
         }
 
-        mRussians = russians;
-        mLatins = latins;
+        for (int i = 0; i < russians.length; i++) {
+            russianToLatin.put(russians[i].charAt(0), latins[i].charAt(0));
+        }
+
+
     }
 
     public String convert(CharSequence source) {
@@ -19,14 +25,10 @@ public class PasswordsHelper {
 
         for (int i = 0; i < source.length(); i++) {
             char c = source.charAt(i);
-            String key = String.valueOf(Character.toLowerCase(c));
+            char cLow = Character.toLowerCase(c);
 
-            for (int dict = 0; dict < mRussians.length; dict++) {
-                if (key.equals(mRussians[dict])) {
-                    result.append(Character.isUpperCase(c) ?
-                            mLatins[dict].toUpperCase() : mLatins[dict]);
-                }
-            }
+            result.append(Character.isUpperCase(c) ?
+                    Character.toUpperCase(russianToLatin.get(cLow)) : russianToLatin.get(cLow));
 
             if (result.length() <= i) {
                 result.append(c);
