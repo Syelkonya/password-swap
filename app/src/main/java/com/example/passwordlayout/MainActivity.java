2c24c602225dfe9bda83fc5bbb81b7bb604590e0
Syelkonya <55093905+Syelkonya@users.noreply.github.com>
2019-11-18 19:31:26 +0300

    Выглядит как после пары 16.11.19

--- app/src/main/java/com/example/passwordlayout/MainActivity.java
+++ app/src/main/java/com/example/passwordlayout/MainActivity.java
@@ -9,41 +9,47 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private TextView mResultTextView;
-    private EditText mSourceTextView;
-    private View mCopyButton;
-    private PasswordsHelper mHelper;
+    private TextView resultTextView;
+    private EditText sourceTextView;
+    private View copyButton;
+    private PasswordsHelper helper;
+    private ImageView quality;
+    private TextView qualityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mHelper = new PasswordsHelper(
+        helper = new PasswordsHelper(
                 getResources().getStringArray(R.array.russian),
-                getResources().getStringArray((R.array.english))
+                getResources().getStringArray(R.array.english)
         );
 
 
-        mResultTextView = findViewById(R.id.result_text);
-        mSourceTextView = findViewById(R.id.source_text);
-        mCopyButton = findViewById(R.id.button_copy);
+        resultTextView = findViewById(R.id.result_text);
+        sourceTextView = findViewById(R.id.source_text);
+        copyButton = findViewById(R.id.button_copy);
+        quality = findViewById(R.id.quality);
+        qualityTextView = findViewById(R.id.quality_text);
 
-        mCopyButton.setEnabled(false);
 
-        mCopyButton.setOnClickListener(new View.OnClickListener() {
+        copyButton.setEnabled(false);
+
+        copyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ClipboardManager manager =
                         (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 manager.setPrimaryClip(ClipData.newPlainText(
                         MainActivity.this.getString(R.string.clipboard_title),
-                        mResultTextView.getText()
+                        resultTextView.getText()
                 ));
 
                 Toast.makeText(MainActivity.this,
@@ -54,7 +60,7 @@ public class MainActivity extends Activity {
         });
 
 
-        mSourceTextView.addTextChangedListener(new TextWatcher() {
+        sourceTextView.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -62,8 +68,11 @@ public class MainActivity extends Activity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mResultTextView.setText(mHelper.convert(s));
-                mCopyButton.setEnabled(s.length() > 0);
+                resultTextView.setText(helper.convert(s));
+                copyButton.setEnabled(s.length() > 0);
+                int qualityInt = helper.getQuality(s);
+                quality.setImageLevel(qualityInt*1000);
+                qualityTextView.setText(getResources().getStringArray(R.array.qualities)[qualityInt]);
             }
 
             @Override
--- app/src/main/java/com/example/passwordlayout/PasswordsHelper.java
+++ app/src/main/java/com/example/passwordlayout/PasswordsHelper.java
@@ -8,15 +8,15 @@ public class PasswordsHelper {
 
     private final Map<Character, Character> russianToLatin = new HashMap<>();
 
+
     public PasswordsHelper(String[] russians, String[] latins) {
-        if (russians.length != latins.length) {
+        if ((russians.length != latins.length)) {
             throw new IllegalArgumentException();
         }
 
         for (int i = 0; i < russians.length; i++) {
             russianToLatin.put(russians[i].charAt(0), latins[i].charAt(0));
         }
-
 
     }
 
@@ -27,13 +27,20 @@ public class PasswordsHelper {
             char c = source.charAt(i);
             char cLow = Character.toLowerCase(c);
 
-            result.append(Character.isUpperCase(c) ?
-                    Character.toUpperCase(russianToLatin.get(cLow)) : russianToLatin.get(cLow));
+            if (russianToLatin.get(cLow) != null) {
+                result.append(Character.isUpperCase(c) ?
+                        Character.toUpperCase(russianToLatin.get(cLow)) : russianToLatin.get(cLow));
+            }else result.append(c);
+
 
             if (result.length() <= i) {
                 result.append(c);
             }
         }
         return result.toString();
+    }
+
+    public int getQuality (CharSequence password){
+        return Math.min(password.length(),10);
     }
 }
